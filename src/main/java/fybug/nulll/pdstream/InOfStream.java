803601a4944b23cda5ed66426a23341263256de9
fybug <1006291762@qq.com>
2019-11-20 01:22:57 +0800

    整合接口

---
+++ src/main/java/fybug/nulll/pdstream/InOfStream.java
@@ -0,0 +1,61 @@
+package fybug.nulll.pdstream;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+/**
+ * <h2>面向流的读取操作工具.</h2>
+ * 该类操作器用于操作流
+ * <p>
+ * {@link #read(int)} 在不同的流作为操作对象时会有语义上的差别，请查看文档
+ *
+ * @author fybug
+ * @version 0.0.1
+ * @since PDStream restart-0.0.1
+ */
+public
+interface InOfStream<O extends Closeable, D> extends Operator<O> {
+    /**
+     * 读取操作对象中的所有数据
+     *
+     * @return all data
+     */
+    @Nullable
+    default
+    D read() { return read(Integer.MAX_VALUE); }
+
+    /** 同时执行 {@link #read()} 和 {@link #close()} */
+    @Nullable
+    default
+    D readAndClose() {
+        synchronized ( this ){
+            try {
+                return read();
+            } finally {
+                close();
+            }
+        }
+    }
+
+    /**
+     * 读取指定长度的数据
+     *
+     * @param size 数据长度
+     *
+     * @return data
+     */
+    @Nullable
+    D read(int size);
+
+    @Override
+    default
+    void close() {
+        try {
+            synchronized ( this ){
+                original().close();
+            }
+        } catch ( IOException ignored ) {
+        }
+    }
+}
---
+++ src/main/java/fybug/nulll/pdstream/OperatorTOOL.java
@@ -0,0 +1,160 @@
+package fybug.nulll.pdstream;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+/**
+ * <h2>PDStream 附加工具包.</h2>
+ * 主要用于增强该包的功能
+ * 并用于衔接不同操作器间的数据操作
+ *
+ * @author fybug
+ * @version 0.0.1
+ * @since PDStream restart-0.0.1
+ */
+public final
+class OperatorTOOL {
+    @Deprecated
+    private
+    OperatorTOOL() {}
+
+    // todo 行解析
+    // todo 自定义解析
+
+    /**
+     * 字节解析为字符串
+     *
+     * @param bytes byte
+     *
+     * @return 传入空数据返回 {@code ""}
+     */
+    @NotNull
+    public static
+    String byteOf(@Nullable byte[] bytes) {
+        if (bytes == null || bytes.length == 0)
+            return "";
+        return new String(bytes, StandardCharsets.UTF_8);
+    }
+
+    /**
+     * 整合字符并转化为字节
+     * <p>
+     * 列表中一个元素将视为一行字符串
+     * 在整合过程中会进行 {@link String#trim()}
+     * 空字符串不会被整合
+     *
+     * @param strings 字符串列表
+     *
+     * @return byte
+     */
+    @NotNull
+    public static
+    byte[] ofByte(@NotNull List<String> strings) {
+        if (strings.size() == 0)
+            return new byte[0];
+        if (strings.size() == 1 && strings.get(0) != null)
+            return strings.get(0).getBytes();
+
+        var stringbuilder = new StringBuilder();
+
+        strings.forEach(v -> {
+            if (v != null && !v.trim().isEmpty())
+                stringbuilder.append(v.trim()).append(System.lineSeparator());
+        });
+
+        return stringbuilder.toString().trim().getBytes();
+    }
+
+    /**
+     * 整合为字符串并转化为字节
+     * <p>
+     * 仅将字符串拼接并转换
+     *
+     * @param s 转化的字符串
+     *
+     * @return byte
+     */
+    @NotNull
+    public static
+    byte[] ofByte(String... s) {
+        if (s.length == 0)
+            return new byte[0];
+        if (s.length == 1 && s[0] != null)
+            return s[0].getBytes();
+
+        var stringBuilder = new StringBuilder();
+
+        for ( String nows : s ){
+            if (nows != null)
+                stringBuilder.append(nows);
+        }
+
+        return stringBuilder.toString().getBytes();
+    }
+
+    /**
+     * 反序列化
+     * <p>
+     * 空数据会返回 {@code null}
+     *
+     * @param bytes byte
+     *
+     * @return 反序列化后的对象
+     */
+    @Nullable
+    public static
+    java.io.Serializable byteSerializable(@Nullable byte[] bytes) {
+        if (bytes == null || bytes.length == 0)
+            return null;
+
+        var buffRead = new ByteArrayInputStream(bytes);
+
+        try {
+            var objRead = new ObjectInputStream(buffRead);
+
+            var ob = objRead.readObject();
+            objRead.close();
+
+            return (Serializable) ob;
+        } catch ( IOException | ClassNotFoundException e ) {
+            return null;
+        }
+    }
+
+    /**
+     * 序列化对象为字节
+     *
+     * @param o 可序列化对象
+     *
+     * @return byte
+     */
+    @NotNull
+    public static
+    byte[] serializable(@Nullable java.io.Serializable o) {
+        if (o == null)
+            return new byte[0];
+        if (o instanceof String)
+            return ofByte((String) o);
+
+        var buff = new ByteArrayOutputStream();
+
+        try {
+            var objout = new ObjectOutputStream(buff);
+
+            objout.writeObject(o);
+            objout.close();
+
+            return buff.toByteArray();
+        } catch ( IOException e ) {
+            return new byte[0];
+        }
+    }
+}
